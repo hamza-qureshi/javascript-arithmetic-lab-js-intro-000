b9f3db84faa809bfaa2d9e9afdcd5213fed870a5
Hamza Qureshi <hamza.qureshi97@gmail.com>
2018-09-26 18:18:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,18 @@ function dec(n){
   return n
 }
 function makeInt(n){
+<<<<<<< HEAD
   return  parseInt(n,10)
 return parseInt('Int function',10)
 }
 function preserveDecimal(n){
   return parseFloat(n)
+=======
+return  parseInt(n,10)
+return parseInt('Int function',10)
+}
+function preserveDecimal(n){
+return parseFloat('2.222')
+return parseFloat('Int function')
+>>>>>>> f25a15c8f8a6584d35b2497d10bba5924bb40df4
 }
